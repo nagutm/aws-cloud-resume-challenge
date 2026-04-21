72f98786eede9b1725f5bc5771e1442e691d7e30
Mufaddal Naguthanawala <m.naguthana@hotmail.com>
2026-04-20 22:22:17 -0400

    add index object for s3 bucket

--- infrastructure/main/main.tf
+++ infrastructure/main/main.tf
@@ -1,3 +1,11 @@
 resource "aws_s3_bucket" "site" {
   bucket = var.bucket_name
 }
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.site.id
+  key    = "index.html"
+  source = "${path.module}/../../frontend/index.html"
+  etag = filemd5("${path.module}/../../frontend/index.html")
+  content_type = "text/html"
+}
