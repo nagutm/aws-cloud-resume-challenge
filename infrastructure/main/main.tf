501138ca389ea13f91f6e912432b615fe976158e
Mufaddal Naguthanawala <m.naguthana@hotmail.com>
2026-04-22 18:29:06 -0400

    add cloudfront distribution, OAC and update bucket policy

--- infrastructure/main/main.tf
+++ infrastructure/main/main.tf
@@ -9,3 +9,64 @@ resource "aws_s3_object" "index" {
   etag = filemd5("${path.module}/../../frontend/index.html")
   content_type = "text/html"
 }
+
+resource "aws_cloudfront_origin_access_control" "site" {
+  name                              = "${var.bucket_name}-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "site" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  # Where the content comes from
+  origin {
+    domain_name              = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id                = "s3-site"
+    origin_access_control_id = aws_cloudfront_origin_access_control.site.id
+  }
+
+  # How CloudFront should cache and serve the content
+  default_cache_behavior {
+    target_origin_id       = "s3-site"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies { forward = "none" }
+    }
+  }
+
+  # No restrictions on where the site can be accessed from
+  restrictions {
+    geo_restriction { restriction_type = "none" }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# Allows CloudFront services to read from the S3 bucket
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.site.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "cloudfront.amazonaws.com" }
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.site.arn}/*"
+      Condition = {
+        StringEquals = {
+          "AWS:SourceArn" = aws_cloudfront_distribution.site.arn
+        }
+      }
+    }]
+  })
+}
+
--- infrastructure/main/outputs.tf
+++ infrastructure/main/outputs.tf
@@ -5,3 +5,7 @@ output "bucket_name" {
 output "bucket_arn" {
   value = aws_s3_bucket.site.arn
 }
+
+output "cloudfront_domain" {
+  value = aws_cloudfront_distribution.site.domain_name
+}
